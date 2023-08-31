5c42a4dc2a078fee6cd1e020d94afa65ffd60dee
Rodrigo Lessa <rodlessar@gmail.com>
2023-08-31 00:58:15 -0300

    update

--- server/src/employee.routes.ts
+++ server/src/employee.routes.ts
@@ -23,10 +23,10 @@ employeeRouter.get("/:id", async (req, res) => {
         if (employee) {
             res.status(200).send(employee);
         } else {
-            res.status(404).send(`Failed to find an employee: ID ${id}`);
+            res.status(404).send(`Falha ao encontrar: ID ${id}`);
         }
     } catch (error) {
-        res.status(404).send(`Failed to find an employee: ID ${req?.params?.id}`);
+        res.status(404).send(`Falha ao encontrar: ID ${req?.params?.id}`);
     }
 });
 
@@ -36,9 +36,9 @@ employeeRouter.post("/", async (req, res) => {
         const result = await collections.employees.insertOne(employee);
 
         if (result.acknowledged) {
-            res.status(201).send(`Created a new employee: ID ${result.insertedId}.`);
+            res.status(201).send(`Criado nova entrada: ID ${result.insertedId}.`);
         } else {
-            res.status(500).send("Failed to create a new employee.");
+            res.status(500).send("Falha ao adicionar nova entrada.");
         }
     } catch (error) {
         console.error(error);
@@ -54,11 +54,11 @@ employeeRouter.put("/:id", async (req, res) => {
         const result = await collections.employees.updateOne(query, { $set: employee });
 
         if (result && result.matchedCount) {
-            res.status(200).send(`Updated an employee: ID ${id}.`);
+            res.status(200).send(`Atualizado: ID ${id}.`);
         } else if (!result.matchedCount) {
-            res.status(404).send(`Failed to find an employee: ID ${id}`);
+            res.status(404).send(`Failed to find an transportadora: ID ${id}`);
         } else {
-            res.status(304).send(`Failed to update an employee: ID ${id}`);
+            res.status(304).send(`Failed to update an transportadora: ID ${id}`);
         }
     } catch (error) {
         console.error(error.message);
@@ -73,11 +73,11 @@ employeeRouter.delete("/:id", async (req, res) => {
         const result = await collections.employees.deleteOne(query);
 
         if (result && result.deletedCount) {
-            res.status(202).send(`Removed an employee: ID ${id}`);
+            res.status(202).send(`Removido uma entrada: ID ${id}`);
         } else if (!result) {
-            res.status(400).send(`Failed to remove an employee: ID ${id}`);
+            res.status(400).send(`Falha ao remover: ID ${id}`);
         } else if (!result.deletedCount) {
-            res.status(404).send(`Failed to find an employee: ID ${id}`);
+            res.status(404).send(`Falha ao buscar: ID ${id}`);
         }
     } catch (error) {
         console.error(error.message);
