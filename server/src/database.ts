5c3ef11de43576d97025af4cffa3c6479cd993f3
Rodrigo Lessa <rodlessar@gmail.com>
2023-08-31 00:55:16 -0300

    update

--- server/src/database.ts
+++ server/src/database.ts
@@ -22,7 +22,7 @@ async function applySchemaValidation(db: mongodb.Db) {
     const jsonSchema = {
         $jsonSchema: {
             bsonType: "object",
-            required: ["name", "position", "level"],
+            required: ["name",],
             additionalProperties: false,
             properties: {
                 _id: {},
@@ -30,16 +30,7 @@ async function applySchemaValidation(db: mongodb.Db) {
                     bsonType: "string",
                     description: "'name' is required and is a string",
                 },
-                position: {
-                    bsonType: "string",
-                    description: "'position' is required and is a string",
-                    minLength: 5
-                },
-                level: {
-                    bsonType: "string",
-                    description: "'level' is required and is one of 'junior', 'mid', or 'senior'",
-                    enum: ["junior", "mid", "senior"],
-                },
+
             },
         },
     };
