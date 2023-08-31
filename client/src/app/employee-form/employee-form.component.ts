ba661a89414cb610c45848444fbf45b10f47ef45
Rodrigo Lessa <rodlessar@gmail.com>
2023-08-31 00:53:26 -0300

    update

--- client/src/app/employee-form/employee-form.component.ts
+++ client/src/app/employee-form/employee-form.component.ts
@@ -21,36 +21,6 @@ import { Employee } from '../employee';
         </div>
       </div>
 
-      <div class="form-floating mb-3">
-        <input class="form-control" type="text" formControlName="position" placeholder="Position" required>
-        <label for="position">Position</label>
-      </div>
-
-      <div *ngIf="position.invalid && (position.dirty || position.touched)" class="alert alert-danger">
-
-        <div *ngIf="position.errors?.['required']">
-          Position is required.
-        </div>
-        <div *ngIf="position.errors?.['minlength']">
-          Position must be at least 5 characters long.
-        </div>
-      </div>
-
-      <div class="mb-3">
-        <div class="form-check">
-          <input class="form-check-input" type="radio" formControlName="level" name="level" id="level-junior" value="junior" required>
-          <label class="form-check-label" for="level-junior">Junior</label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" formControlName="level" name="level" id="level-mid" value="mid">
-          <label class="form-check-label" for="level-mid">Mid</label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" formControlName="level" name="level" id="level-senior"
-            value="senior">
-          <label class="form-check-label" for="level-senior">Senior</label>
-        </div>
-      </div>
 
       <button class="btn btn-primary" type="submit" [disabled]="employeeForm.invalid">Add</button>
     </form>
@@ -78,15 +48,11 @@ export class EmployeeFormComponent implements OnInit {
   constructor(private fb: FormBuilder) { }
 
   get name() { return this.employeeForm.get('name')!; }
-  get position() { return this.employeeForm.get('position')!; }
-  get level() { return this.employeeForm.get('level')!; }
 
   ngOnInit() {
     this.initialState.subscribe(employee => {
       this.employeeForm = this.fb.group({
         name: [ employee.name, [Validators.required, Validators.minLength(3) ] ],
-        position: [ employee.position, [ Validators.required, Validators.minLength(5) ] ],
-        level: [ employee.level, [Validators.required] ]
       });
     });
 
